c1753653ae7d6c0618d30fce5978eb5249f47166
Shubhanshu <Shubhanshusingh179@gmail.com>
2022-03-03 18:35:25 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import AgeCalculator from './AgeCalculator';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <AgeCalculator />
+
+    </div>
+  );
+}
+
+export default App;
